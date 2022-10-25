ee5826a858ee66e163b379b67761d24ed86e7d75
smartwe <me@smartwe.kr>
2022-10-25 20:31:03 +0900

    U: Readme.md

---
+++ preload.js
@@ -0,0 +1,14 @@
+/**
+ * The preload script runs before. It has access to web APIs
+ * as well as Electron's renderer process modules and some
+ * polyfilled Node.js functions.
+ * 
+ * https://www.electronjs.org/docs/latest/tutorial/sandbox
+ */
+window.addEventListener('DOMContentLoaded', () => {
+  try {
+    document.getElementsByClassName("footer_inner")[0].remove()
+  } catch (error) {
+    
+  }
+})
